c9e671117ffbd6e6e1e16b00e7dce6df2c861c76
Joe Mac <jhmccarney@gmail.com>
2024-10-22 09:56:21 +0100

    vite config for build dir

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()], ssr: { noExternal: [ 'postprocessing', 'three-good-godrays' ] }
+	plugins: [sveltekit()], ssr: { noExternal: [ 'postprocessing', 'three-good-godrays' ] },
+	build: {
+		outDir: 'dist',
+	  },
 });
