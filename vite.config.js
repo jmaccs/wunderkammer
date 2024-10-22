01c0e5e9777085aa47c39fd02249d8326568402e
Joe Mac <jhmccarney@gmail.com>
2024-10-22 10:32:32 +0100

    prerender config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()], ssr: { noExternal: [ 'postprocessing', 'three-good-godrays' ] },
+	plugins: [sveltekit()], ssr: { noExternal: [ 'postprocessing' ] },
 	build: {
 		outDir: 'build',
 	  },
